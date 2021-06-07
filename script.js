fc8ae8a1a0485793127e4f0e57f2463deafdf952
itz-shyam <75735908+itz-shyam@users.noreply.github.com>
2021-06-07 10:42:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+function addUser() {
+    player1=document.getElementById("player1").value;
+     player2=document.getElementById("player2").value;
 
+     localStorage.setItem("player1",player1);
+     localStorage.setItem("player2",player2);
+
+     localStorage.setItem("player1score",0);
+     localStorage.setItem("player2score",0);
+
+     window.location="main.html";
+    };
+
+function test(){
+    add11=Number(localStorage.getItem("player1score")) + 1
+    localStorage.setItem("player1score", add11)
+    document.getElementById("p1score").innerHTML=localStorage.getItem("player1score");
+}
 
 function login() {
     var firstplayer = document.getElementById("player1").value
